ab0873281cb7c180ef778c74b2e786975a7ac637
Marie <marie.borgmeier@urs-os.de>
2023-08-18 21:30:39 +0200

    ich werde verrückt!

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,32 @@
 <html>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <body class="image-background">
-        <div class="flex">
-            <a href="#" class="text-white">Log In</a>
+<script src="https://cdn.tailwindcss.com"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<body>
+    <div class="flex">
+        <a href="#" class="">Log In</a>
+    </div>
+    <div class="container my-3 px-3 py-2" id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <!--Carousel items-->
+        <div class="carousel-inner h-5/6 rounded-xl">
+            <!--First item-->
+            <div class="carousel-item active">
+                <img src="./images/basketball.jpg" class="inline w-100" alt="basketball" />
+            </div>
+            <!--Second item-->
+            <div class="carousel-item">
+                <img src="./images/fußball.jpg" class="inline w-100" alt="fußball" />
+            </div>
+            <!--Third item-->
+            <div class="carousel-item">
+                <img src="./images/tischtennis2.jpg" class="inline w-100" alt="tischtennis" />
+            </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
