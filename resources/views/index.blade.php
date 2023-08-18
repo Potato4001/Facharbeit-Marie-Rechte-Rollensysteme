9c3bc815d3b71b922a86af63e0e24094903eaf4f
Marie <marie.borgmeier@urs-os.de>
2023-08-18 15:23:26 +0200

    scheiße

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <body class="image-background">
+        <div class="flex">
+            <a href="#" class="text-white">Log In</a>
+        </div>
+    </body>
+</html>
