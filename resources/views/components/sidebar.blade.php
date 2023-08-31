14505c3370e5da372761b4037cd460055cb95e2c
Marie <marie.borgmeier@urs-os.de>
2023-08-31 20:44:46 +0200

    Wieso ist das so schwer?

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,18 +1,28 @@
-<div class="bg-blue-500 h-screen p-2 pt-8 w-72 relative">
-    <div class="bg-white text-blue-500 rounded-full absolute -right-5 top-10 py-2 px-2 border-2 cursor-pointer">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-        </svg>
-    </div>
-    <div class="inline-flex">
-        <div class="flex flex-col mx-3 mt-5">
-            <a href="./" class="border-b pb-2 inline-flex items-center w-full">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 text-white">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+<div class="relative min-h-screen md:flex" x-data="{ open: true }">
+    <!-- Sidebar -->
+    <aside :class="{ '-translate-x-full': !open }" class="z-10 bg-blue-800 text-blue-100 w-64 px-2 py-4 absolute inset-y-0 left-0 md:relative transform md:translate-x-0
+                overflow-y-auto transition ease-in-out duration-200 shadow-lg">
+        <!--Logo-->
+        <div class="flex items-center justify-between px-2">
+            <div class="flex items-center space-x-2">
+                <a href="./" class="border-b pb-2 inline-flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                    </svg>
+                    <span class="text-lg ml-2 font-bold font-mono">TeamAssemble</span>
+                </a>
+            </div>
+            <button type="button" @click="open = ! open" class="inline-flex p-2 items-center justify-center rounded-md text-blue-100 hover:bg-blue-700
+                focus:outline-none">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
-                <span class="text-lg ml-2 font-bold font-mono">TeamAssemble</span>
-            </a>
+            </button>
+        </div>
 
+        <!-- Nav links -->
+        <div>
+            <!--
             <ul class="mt-10">
                 <li class="mt-3 ml-1 border-b pb-2 inline-flex items-center w-full">
                     <a href="./team" class="inline-flex items-center text-lg font-bold">
@@ -42,6 +52,29 @@
                 </li>
                 @endif
             </ul>
+-->
         </div>
-    </div>
+    </aside>
+
+    <!-- Main content -->
+    <main>
+        <nav class="bg-glue-900">
+            <div class="mx-auto px-2 sm:px-6 lg:px-8">
+                <div class="relative flex items-center justify-between md:justify-end h-16">
+                    <div class="absolute inset-y-0 left-0 flex items-center md:hidden">
+                        <button type="button" @click="open = !open" @click.away="open = false" class="inline-flex items-center justify-center
+                            p-2 rounded-md text-blue-100 hover:bg-blue-700 focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div>
+            {{ $slot }}
+        </div>
+    </main>
 </div>
--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -1,24 +1,24 @@
 <!doctype html>
 <script src="https://cdn.tailwindcss.com"></script>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 <body>
-    <div class="flex">
-        <x-sidebar/>
-
-        <div class="flex items-center flex-column mx-auto">
-            <nav class="inline-flex items-center mb-20">
+    <x-sidebar>
+        <div class="">
+            <nav class="">
                 <div class="">
                     <h1 class="text-lg font-bold">Create new Team</h1>
                 </div>
                 <div class="">
                     <a href="./" class="">
-                        <img src="./images/logo.png" alt="logo" width="165" height="16">
+                        <img src="./images/logo.png" alt="logo" width="165" height="16" />
                     </a>
                 </div>
             </nav>
@@ -26,14 +26,15 @@
                 <form method="POST" action="team" class="mt-10">
                     @csrf
 
-                    <x-form.input name=" team name"/>
+                    <x-form.input name=" team name" />
 
-                    <x-form.input name="sport type"/>
+                    <x-form.input name="sport type" />
 
                     <x-form.button>Log In</x-form.button>
                 </form>
             </x-panel>
         </div>
+    </x-sidebar>
 
-    </div>
+
 </body>
