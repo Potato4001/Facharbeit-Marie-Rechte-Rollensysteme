d7faf2495cfa25b2808efc66b8986e78bde58da1
Marie <marie.borgmeier@urs-os.de>
2023-09-10 19:21:26 +0200

    papaa

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -21,60 +21,78 @@
 </head>
 
 <body>
-    <div class="relative min-h-screen md:flex" x-data="{ open: false }">
+    <div class="relative min-h-screen md:flex">
         <!-- Sidebar -->
-        <aside :class="open ? '' : 'hidden'" class="z-10 w-64 px-2 py-4 absolute inset-y-0 left-0 md:relative transform md:translate-x-0
-                overflow-y-auto transition ease-in-out duration-200 shadow-lg">
+        <aside class="z-10 w-16 absolute inset-y-0 left-0 md:relative transform md:translate-x-0
+                overflow-y-auto hover:duration-500 shadow-lg hover:w-64">
             <!--Logo vom Verein soll hier bekannt gemacht werden + der Name des Vereins + Subtitle Name des Spielers-->
-            <div class="flex items-center justify-between px-2 bg-blue-800 text-blue-100">
-                <div class="flex items-center space-x-2 mt-2">
-                    <a href="./" class="border-b pb-2 inline-flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <div class="flex items-center justify-between px-3 bg-blue-800 text-blue-100 py-2">
+                <div class="flex items-center space-x-2">
+                    <a href="./" class="inline-flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                         </svg>
-                        <span class="text-lg ml-2 font-fantasy">TeamAssemble</span>
+                        <span class="text-lg ml-5 font-fantasy">TeamAssemble</span>
                     </a>
                 </div>
-                <button type="button" @click="open = ! open" class="ml-2 inline-flex p-2 items-center justify-center rounded-md text-blue-100 hover:bg-blue-700
-                focus:outline-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                    </svg>
-
-                </button>
             </div>
 
             <!-- Nav links -->
             <div>
 
                 <ul class="mt-10">
-                    <li class="mt-3 ml-1 border-b pb-2 inline-flex items-center w-40">
+                    <li class="mt-3 ml-4 border-b pb-2 inline-flex items-center w-48">
                         <a href="./team" class="inline-flex items-center text-lg font-fantasy">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 mr-2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 mr-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                             </svg>
-                            Create Team
+
+                            <span class="mt-2">Home</span>
                         </a>
                     </li>
 
-                    <li class="mt-3 ml-1 border-b pb-2 inline-flex items-center w-40">
+                    <li class="mt-3 ml-4 border-b pb-2 inline-flex items-center w-48">
                         <a href="./join" class="inline-flex items-center text-lg font-fantasy {{ request()->is('join')|request()->is('join/register') ? 'text-blue-400' : '' }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 mr-2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 mr-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                             </svg>
-                            Register
+
+                            <span class="mt-2">Events</span>
                         </a>
                     </li>
 
-                    <li class="mt-3 ml-1 border-b pb-2 inline-flex items-center w-40">
+                    <li class="mt-3 ml-4 border-b pb-2 inline-flex items-center w-48">
                         <a href="./login" class="inline-flex items-center text-lg font-fantasy">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 mr-2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 mr-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                            Log In
+
+                            <span>Treasury</span>
                         </a>
                     </li>
 
+                    <li class="mt-3 ml-4 inline-flex items-center w-48">
+                        <a href="./setting" class="font-fantasy inline-flex items-center text-lg">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 mr-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            </svg>
+
+                            <span>Setting</span>
+                        </a>
+                    </li>
+
+
+                    <li class="mt-3 ml-4 inline-flex items-center w-48">
+                        <a href="./logout" class="font-fantasy inline-flex items-center text-lg">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 mr-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9" />
+                            </svg>
+
+
+                            <span>Log Out</span>
+                        </a>
+                    </li>
                 </ul>
 
             </div>
@@ -85,13 +103,7 @@
                 <!-- Jumbotron -->
                 <div class="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-12 text-center" style="background-image: url('images/background-image.png'); height: 400px">
                     <div class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed" style="background-color: rgba(0, 0, 0, 0.6)">
-                        <div class="flex h-full items-center justify-between">
-                            <button type="button" @click="open = ! open" @click.away="open = false" class="inline-flex justify-center
-                                p-2 rounded-md text-blue-100 hover:bg-blue-700 focus:outline-none ml-8">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                                </svg>
-                            </button>
+                        <div class="flex h-full items-center justify-center">
                             <div class="text-white">
                                 <h2 class="mb-4 text-4xl font-semibold">Home</h2>
                                 <h4 class="mb-6 text-xl font-semibold"></h4>
